6ce6d8829a9f8a19830d4411a38eef8d30c352e4
caikaisheng <caikaisheng@wepie.com>
2023-05-10 10:08:17 +0800

    fix: doc config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,37 @@
 // docs/.vuepress/config.js
+const nav = require("./nav.js");
 module.exports = {
-    title: "科技趣玩",
-    description: "分享各类资源、教程、黑科技软件、工具等等",
+    title: "wabicai的个人空间",
+    description: "前端、后端、互联网前沿学习等等",
     markdown: {
         lineNumbers: true,
     },
     plugins: [
         ["vuepress-plugin-auto-sidebar", {
             // options
-            sort: {
-                mode: "asc",
-                readmeFirst: true,
-                readmeFirstForce: false
-            },
-            title: {
-                mode: "titlecase",
-                map: {}
-            },
-            sidebarDepth: 1,
-            collapse: {
-                open: false,
-                collapseList: [],
-                uncollapseList: []
-            },
-            ignore: [],
-            removeEmptyGroup: false,
-            git: {
-                trackStatus: 'all'
-            }
+            // sort: {
+            //     mode: "asc",
+            //     readmeFirst: true,
+            //     readmeFirstForce: false
+            // },
+            // title: {
+            //     mode: "titlecase",
+            //     map: {}
+            // },
+            // sidebarDepth: 1,
+            // collapse: {
+            //     open: false,
+            //     collapseList: [],
+            //     uncollapseList: []
+            // },
+            // ignore: [],
+            // removeEmptyGroup: false,
+            // git: {
+            //     trackStatus: 'all'
+            // }
         }]
     ],
     themeConfig: {
-        sidebar: [
-            {
-                title: '前端',
-                children: ['/fronent-end/test'],
-                initialOpenGroupIndex: 1 // 可选的, 默认值是 0
-            }
-        ]
+        nav
     },
 };
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    "text": "Golang",
+    "items": [
+      {
+        "text": "Golang",
+        "link": "/Golang/"
+      }
+    ]
+  },
+  {
+    "text": "JavaScript",
+    "items": [
+      {
+        "text": "Java Script",
+        "link": "/JavaScript/"
+      }
+    ]
+  }
+];
