5f31c8c65f65fe64b4639afc8e8c6eb5d42ad30e
caikaisheng <caikaisheng@wepie.com>
2023-05-12 23:37:54 +0800

    fix: 调整目录位置

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,155 +1,64 @@
 module.exports = [
   {
-    "text": "Golang",
-    "items": [
-      {
-        "text": "Golang",
-        "link": "/Golang/"
-      }
-    ]
-  },
-  {
-    "text": "HTML、CSS",
-    "items": [
-      {
-        "text": "HTML、CSS",
-        "link": "/HTML、CSS/"
-      },
-      {
-        "text": "移动端",
-        "link": "/HTML、CSS/移动端/"
-      }
-    ]
-  },
-  {
-    "text": "JavaScript",
-    "items": [
-      {
-        "text": "Java Script",
-        "link": "/JavaScript/"
-      },
-      {
-        "text": "基础知识",
-        "link": "/JavaScript/基础知识/"
-      }
-    ]
-  },
-  {
-    "text": "基础知识",
-    "items": [
-      {
-        "text": "操作系统与数据库",
-        "link": "/基础知识/操作系统与数据库/"
-      },
-      {
-        "text": "浏览器相关",
-        "link": "/基础知识/浏览器相关/"
-      },
-      {
-        "text": "计算机网络",
-        "link": "/基础知识/计算机网络/"
-      },
-      {
-        "text": "设计模式",
-        "link": "/基础知识/设计模式/"
-      }
-    ]
-  },
-  {
-    "text": "工作经验记录",
-    "items": [
-      {
-        "text": "2、Base模块",
-        "link": "/工作经验记录/项目总结/Tencent/TRTC/SDK/2、Base模块/"
-      },
-      {
-        "text": "3、Module模块",
-        "link": "/工作经验记录/项目总结/Tencent/TRTC/SDK/3、Module模块/"
-      },
-      {
-        "text": "Business",
-        "link": "/工作经验记录/项目总结/Tencent/TRTC/SDK/3、Module模块/business/"
-      },
-      {
-        "text": "SDK",
-        "link": "/工作经验记录/项目总结/Tencent/TRTC/SDK/"
-      },
-      {
-        "text": "Statistics",
-        "link": "/工作经验记录/项目总结/Tencent/TRTC/SDK/3、Module模块/statistics/"
-      },
-      {
-        "text": "前端工程化",
-        "link": "/工作经验记录/前端工程化/"
-      },
-      {
-        "text": "多媒体实现",
-        "link": "/工作经验记录/项目总结/Tencent/TRTC/多媒体实现/"
-      },
-      {
-        "text": "小程序插件",
-        "link": "/工作经验记录/项目总结/Tencent/小程序插件/"
-      },
-      {
-        "text": "文件加载",
-        "link": "/工作经验记录/项目总结/Tencent/Sketch/文件加载/"
-      },
-      {
-        "text": "方案设计",
-        "link": "/工作经验记录/项目总结/Tencent/Sketch/方案设计/"
-      },
-      {
-        "text": "远程调试",
-        "link": "/工作经验记录/项目总结/Tencent/远程调试/"
-      },
-      {
-        "text": "项目总结",
-        "link": "/工作经验记录/项目总结/"
-      },
-      {
-        "text": "项目方案设计",
-        "link": "/工作经验记录/项目总结/项目方案设计/"
-      }
-    ]
-  },
-  {
-    "text": "框架相关",
+    "text": "前端",
     "items": [
       {
         "text": "Api原理",
-        "link": "/框架相关/Vue/Api原理/"
+        "link": "/前端/框架相关/Vue/Api原理/"
+      },
+      {
+        "text": "HTML&&CSS",
+        "link": "/前端/HTML&&CSS/"
+      },
+      {
+        "text": "Java Script",
+        "link": "/前端/JavaScript/"
       },
       {
         "text": "React",
-        "link": "/框架相关/React/"
+        "link": "/前端/框架相关/React/"
       },
       {
-        "text": "Self Vue Master",
-        "link": "/框架相关/Vue/self-vue-master/"
-      },
-      {
-        "text": "V1",
-        "link": "/框架相关/Vue/self-vue-master/v1/"
-      },
-      {
-        "text": "V2",
-        "link": "/框架相关/Vue/self-vue-master/v2/"
-      },
-      {
-        "text": "V3",
-        "link": "/框架相关/Vue/self-vue-master/v3/"
-      },
-      {
-        "text": "Vue",
-        "link": "/框架相关/Vue/"
+        "text": "基础知识",
+        "link": "/前端/JavaScript/基础知识/"
       },
       {
         "text": "小程序",
-        "link": "/框架相关/小程序/"
+        "link": "/前端/框架相关/小程序/"
       },
       {
-        "text": "框架相关",
-        "link": "/框架相关/"
+        "text": "用法",
+        "link": "/前端/框架相关/Vue/用法/"
+      }
+    ]
+  },
+  {
+    "text": "后端",
+    "items": [
+      {
+        "text": "Golang",
+        "link": "/后端/Golang/"
+      }
+    ]
+  },
+  {
+    "text": "基石",
+    "items": [
+      {
+        "text": "操作系统与数据库",
+        "link": "/基石/操作系统与数据库/"
+      },
+      {
+        "text": "浏览器相关",
+        "link": "/基石/浏览器相关/"
+      },
+      {
+        "text": "计算机网络",
+        "link": "/基石/计算机网络/"
+      },
+      {
+        "text": "设计模式",
+        "link": "/基石/设计模式/"
       }
     ]
   },
@@ -157,16 +66,12 @@ module.exports = [
     "text": "面试记录",
     "items": [
       {
-        "text": "Self Promise",
-        "link": "/面试记录/前端手写Api相关/self-promise/"
-      },
-      {
         "text": "前端手写Api相关",
         "link": "/面试记录/前端手写Api相关/"
       },
       {
-        "text": "面试记录",
-        "link": "/面试记录/"
+        "text": "场景题",
+        "link": "/面试记录/场景题/"
       }
     ]
   }
