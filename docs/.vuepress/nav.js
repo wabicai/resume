d7c779cba40d721435c540a648eaa992b1d0c046
caikaisheng <caikaisheng@wepie.com>
2023-05-12 23:47:19 +0800

    fix: doc

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -3,32 +3,36 @@ module.exports = [
     "text": "前端",
     "items": [
       {
-        "text": "Api原理",
-        "link": "/前端/框架相关/Vue/Api原理/"
-      },
-      {
-        "text": "HTML&&CSS",
-        "link": "/前端/HTML&&CSS/"
+        "text": "基础知识",
+        "link": "/前端/JavaScript/基础知识/"
       },
       {
         "text": "Java Script",
         "link": "/前端/JavaScript/"
       },
       {
-        "text": "React",
-        "link": "/前端/框架相关/React/"
+        "text": "Api原理",
+        "link": "/前端/Vue/Api原理/"
       },
       {
-        "text": "基础知识",
-        "link": "/前端/JavaScript/基础知识/"
-      },
-      {
-        "text": "小程序",
-        "link": "/前端/框架相关/小程序/"
+        "text": "HTML、CSS",
+        "link": "/前端/HTML、CSS/"
       },
       {
         "text": "用法",
-        "link": "/前端/框架相关/Vue/用法/"
+        "link": "/前端/Vue/用法/"
+      },
+      {
+        "text": "React",
+        "link": "/前端/React/"
+      },
+      {
+        "text": "浏览器相关",
+        "link": "/前端/浏览器相关/"
+      },
+      {
+        "text": "小程序",
+        "link": "/前端/小程序/"
       }
     ]
   },
@@ -45,12 +49,12 @@ module.exports = [
     "text": "基石",
     "items": [
       {
-        "text": "操作系统与数据库",
-        "link": "/基石/操作系统与数据库/"
+        "text": "git、shell、npm、IDE相关操作",
+        "link": "/基石/git、shell、npm、IDE相关操作/"
       },
       {
-        "text": "浏览器相关",
-        "link": "/基石/浏览器相关/"
+        "text": "操作系统与数据库",
+        "link": "/基石/操作系统与数据库/"
       },
       {
         "text": "计算机网络",
@@ -63,15 +67,60 @@ module.exports = [
     ]
   },
   {
-    "text": "面试记录",
+    "text": "工作记录",
+    "items": [
+      {
+        "text": "2、Base模块",
+        "link": "/工作记录/TRTC/SDK/2、Base模块/"
+      },
+      {
+        "text": "3、Module模块",
+        "link": "/工作记录/TRTC/SDK/3、Module模块/"
+      },
+      {
+        "text": "Business",
+        "link": "/工作记录/TRTC/SDK/3、Module模块/business/"
+      },
+      {
+        "text": "SDK",
+        "link": "/工作记录/TRTC/SDK/"
+      },
+      {
+        "text": "Statistics",
+        "link": "/工作记录/TRTC/SDK/3、Module模块/statistics/"
+      },
+      {
+        "text": "多媒体实现",
+        "link": "/工作记录/TRTC/多媒体实现/"
+      },
+      {
+        "text": "小程序插件",
+        "link": "/工作记录/小程序插件/"
+      },
+      {
+        "text": "工作记录",
+        "link": "/工作记录/"
+      },
+      {
+        "text": "远程调试",
+        "link": "/工作记录/远程调试/"
+      },
+      {
+        "text": "项目方案设计",
+        "link": "/工作记录/项目方案设计/"
+      }
+    ]
+  },
+  {
+    "text": "面经",
     "items": [
       {
         "text": "前端手写Api相关",
-        "link": "/面试记录/前端手写Api相关/"
+        "link": "/面经/前端手写Api相关/"
       },
       {
         "text": "场景题",
-        "link": "/面试记录/场景题/"
+        "link": "/面经/场景题/"
       }
     ]
   }
