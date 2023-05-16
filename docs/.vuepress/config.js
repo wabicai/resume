981024a4421947a2791d0fc90fb70b22566176d4
caikaisheng <caikaisheng@wepie.com>
2023-05-16 19:09:36 +0800

    feat: 浏览器

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,87 +1,94 @@
 // docs/.vuepress/config.js
 const nav = require("./nav.js");
 module.exports = {
-    title: "wabicai的个人空间",
-    description: "前端、后端、互联网前沿学习等等",
-    markdown: {
-        lineNumbers: true,
-        extendMarkdown: md => {
-            md.use(require("markdown-it-disable-url-encode"));
+  title: "wabicai的个人空间",
+  description: "前端、后端、互联网前沿学习等等",
+  markdown: {
+    lineNumbers: true,
+    extendMarkdown: (md) => {
+      md.use(require("markdown-it-disable-url-encode"));
+    },
+  },
+
+  configureWebpack: {
+    plugins: [],
+    resolve: {
+      alias: {
+        "@alias": "../img",
+      },
+    },
+  },
+  // chainWebpack: config => {
+  //     config.module.rules.delete('eslint');
+  //     config.optimization.delete('splitChunks');
+
+  //     config.plugin('define').tap(args => {
+  //         args[0].__VUE_OPTIONS_API__ = true;
+  //         args[0].__VUE_PROD_DEVTOOLS__ = false;
+  //         return args;
+  //     });
+
+  //     config.plugin('ssr').use(ServerRenderPlugin, [{
+  //         filename: 'server-bundle.json'
+  //     }]);
+
+  //     config.plugin('spa').use(HtmlWebpackPlugin, [{
+  //         title: 'Hello World',
+  //         template: path.resolve(__dirname, '.vuepress/public/index.html')
+  //     }]);
+
+  //     config.plugin('dynamic-import-node').use(DynamicImportServer, [{
+  //         filename: 'server-bundle.js'
+  //     }]);
+  // },
+  // ssrTemplate: path.resolve(__dirname, './src/ssr.html'),
+  // chainMarkdown(config) {
+  //     config
+  //         .plugin('markdown-it')
+  //         .tap((options) => {
+  //             options.push(mdContainer, anchorPlugin, [mdKatexPlugin, { delimiters: 'dollars' }]);
+  //             return options;
+  //         });
+  // },
+  vueDemoOptions: {
+    scopedCss: true,
+  },
+  css: {
+    requireModuleExtension: false,
+  },
+  plugins: [
+    [
+      "vuepress-plugin-auto-sidebar",
+      {
+        // options,
+        sort: {
+          mode: "custom",
+          readmeFirst: true,
+          readmeFirstForce: false,
+          fn: (a, b) => {
+            var aNum = parseInt(a.filename.match(/^\d+/)); // 提取a中的数字
+            var bNum = parseInt(b.filename.match(/^\d+/)); // 提取b中的数字
+            return aNum - bNum; // 比较数字大小
+          },
         },
-    },
-
-    configureWebpack: {
-        plugins: [],
-        resolve: {
-            alias: {
-                '@alias': '../img'
-            }
-        }
-    },
-    // chainWebpack: config => {
-    //     config.module.rules.delete('eslint');
-    //     config.optimization.delete('splitChunks');
-
-    //     config.plugin('define').tap(args => {
-    //         args[0].__VUE_OPTIONS_API__ = true;
-    //         args[0].__VUE_PROD_DEVTOOLS__ = false;
-    //         return args;
-    //     });
-
-    //     config.plugin('ssr').use(ServerRenderPlugin, [{
-    //         filename: 'server-bundle.json'
-    //     }]);
-
-    //     config.plugin('spa').use(HtmlWebpackPlugin, [{
-    //         title: 'Hello World',
-    //         template: path.resolve(__dirname, '.vuepress/public/index.html')
-    //     }]);
-
-    //     config.plugin('dynamic-import-node').use(DynamicImportServer, [{
-    //         filename: 'server-bundle.js'
-    //     }]);
-    // },
-    // ssrTemplate: path.resolve(__dirname, './src/ssr.html'),
-    // chainMarkdown(config) {
-    //     config
-    //         .plugin('markdown-it')
-    //         .tap((options) => {
-    //             options.push(mdContainer, anchorPlugin, [mdKatexPlugin, { delimiters: 'dollars' }]);
-    //             return options;
-    //         });
-    // },
-    vueDemoOptions: {
-        scopedCss: true
-    },
-    css: {
-        requireModuleExtension: false
-    },
-    plugins: [
-        ["vuepress-plugin-auto-sidebar", {
-            // options,
-            sort: {
-                mode: "asc",
-                readmeFirst: true,
-                readmeFirstForce: false
-            },
-            title: {
-                mode: "titlecase",
-                map: {}
-            },
-            sidebarDepth: 1,
-            collapse: {
-                open: false,
-                collapseList: [],
-                uncollapseList: []
-            },
-            ignore: [],
-            removeEmptyGroup: false,
-            git: {
-                trackStatus: 'all'
-            }
-        }]
+        title: {
+          mode: "camelcase",
+        },
+        sidebarDepth: 1,
+        collapse: {
+          open: false,
+          collapseList: [],
+          uncollapseList: [],
+        },
+        ignore: [],
+        removeEmptyGroup: false,
+        git: {
+          trackStatus: "all",
+        },
+      },
     ],
-    themeConfig: {
-        nav
-    },
+  ],
+  themeConfig: {
+    nav,
+  },
 };
--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,123 +1,123 @@
 module.exports = [
   {
-    "text": "前端",
-    "items": [
+    text: "前端",
+    items: [
       {
-        "text": "基础知识",
-        "link": "/前端/基础知识/1.静态语言和非静态语言"
+        text: "基础知识",
+        link: "/前端/基础知识/1.静态语音和非静态语音",
       },
       {
-        "text": "HTML、CSS",
-        "link": "/前端/HTML、CSS/1.垂直居中和水平居中方法总结"
+        text: "HTML、CSS",
+        link: "/前端/HTML、CSS/1.垂直居中和水平居中方法总结",
       },
       {
-        "text": "JavaScript",
-        "link": "/前端/JavaScript/1.闭包相关"
+        text: "JavaScript",
+        link: "/前端/JavaScript/1.闭包相关",
       },
       {
-        "text": "浏览器相关",
-        "link": "/前端/浏览器相关/1.渲染进程中的渲染机制"
+        text: "浏览器相关",
+        link: "/前端/浏览器相关/1.渲染进程中的渲染机制",
       },
       {
-        "text": "Vue",
-        "link": "/前端/Vue/1.生命周期，钩子函数"
+        text: "Vue",
+        link: "/前端/Vue/1.生命周期，钩子函数",
       },
       {
-        "text": "React",
-        "link": "/前端/React/1.React Hook"
+        text: "React",
+        link: "/前端/React/1.React Hook",
       },
       {
-        "text": "小程序",
-        "link": "/前端/小程序/1.H5和小程序有什么区别"
-      }
-    ]
+        text: "小程序",
+        link: "/前端/小程序/1.H5和小程序有什么区别",
+      },
+    ],
   },
   {
-    "text": "后端",
-    "items": [
+    text: "后端",
+    items: [
       {
-        "text": "Golang",
-        "link": "/后端/Golang/1.javascript与Golang中类型的异同"
-      }
-    ]
+        text: "Golang",
+        link: "/后端/Golang/1.javascript与Golang中类型的异同",
+      },
+    ],
   },
   {
-    "text": "基石",
-    "items": [
+    text: "基石",
+    items: [
       {
-        "text": "git、shell、npm、IDE相关操作",
-        "link": "/基石/git、shell、npm、IDE相关操作/1.git"
+        text: "git、shell、npm、IDE相关操作",
+        link: "/基石/git、shell、npm、IDE相关操作/1.git",
       },
       {
-        "text": "操作系统与数据库",
-        "link": "/基石/操作系统与数据库/1.进程和线程的区别"
+        text: "操作系统与数据库",
+        link: "/基石/操作系统与数据库/1.进程和线程的区别",
       },
       {
-        "text": "计算机网络",
-        "link": "/基石/计算机网络/1.OSI七层模型，各层协议"
+        text: "计算机网络",
+        link: "/基石/计算机网络/1.OSI七层模型，各层协议",
       },
       {
-        "text": "设计模式",
-        "link": "/基石/设计模式/1.监听者模式和发布订阅模式(EventBus)"
-      }
-    ]
+        text: "设计模式",
+        link: "/基石/设计模式/1.监听者模式和发布订阅模式(EventBus)",
+      },
+    ],
   },
   {
-    "text": "工作记录",
-    "items": [
+    text: "工作记录",
+    items: [
       {
-        "text": "2.Base模块",
-        "link": "/工作记录/TRTC/SDK/2.Base模块/tevent"
+        text: "2.Base模块",
+        link: "/工作记录/TRTC/SDK/2.Base模块/tevent",
       },
       {
-        "text": "3.Module模块",
-        "link": "/工作记录/TRTC/SDK/3.Module模块/1.tlogger"
+        text: "3.Module模块",
+        link: "/工作记录/TRTC/SDK/3.Module模块/1.tlogger",
       },
       {
-        "text": "Business",
-        "link": "/工作记录/TRTC/SDK/3.Module模块/business/business"
+        text: "Business",
+        link: "/工作记录/TRTC/SDK/3.Module模块/business/business",
       },
       {
-        "text": "SDK",
-        "link": "/工作记录/TRTC/SDK/1.tlogger"
+        text: "SDK",
+        link: "/工作记录/TRTC/SDK/1.tlogger",
       },
       {
-        "text": "Statistics",
-        "link": "/工作记录/TRTC/SDK/3.Module模块/statistics/statistics"
+        text: "Statistics",
+        link: "/工作记录/TRTC/SDK/3.Module模块/statistics/statistics",
       },
       {
-        "text": "多媒体实现",
-        "link": "/工作记录/TRTC/多媒体实现/1.RTC方案"
+        text: "多媒体实现",
+        link: "/工作记录/TRTC/多媒体实现/1.RTC方案",
       },
       {
-        "text": "小程序插件",
-        "link": "/工作记录/小程序插件/如何实现插件与小程序的通信"
+        text: "小程序插件",
+        link: "/工作记录/小程序插件/如何实现插件与小程序的通信",
       },
       {
-        "text": "工作记录",
-        "link": "/工作记录/项目难点"
+        text: "工作记录",
+        link: "/工作记录/项目难点",
       },
       {
-        "text": "远程调试",
-        "link": "/工作记录/远程调试/远程调试实现"
+        text: "远程调试",
+        link: "/工作记录/远程调试/远程调试实现",
       },
       {
-        "text": "项目方案设计",
-        "link": "/工作记录/项目方案设计/1.electron xp适配"
-      }
-    ]
+        text: "项目方案设计",
+        link: "/工作记录/项目方案设计/1.electron xp适配",
+      },
+    ],
   },
   {
-    "text": "面经",
-    "items": [
+    text: "面经",
+    items: [
       {
-        "text": "前端手写Api相关",
-        "link": "/面经/前端手写Api相关/1.Promise实现sleep"
+        text: "前端手写Api相关",
+        link: "/面经/前端手写Api相关/1.Promise实现sleep",
       },
       {
-        "text": "场景题",
-        "link": "/面经/场景题/1.大数据列表渲染的优化"
-      }
-    ]
-  }
+        text: "场景题",
+        link: "/面经/场景题/1.大数据列表渲染的优化",
+      },
+    ],
+  },
 ];
